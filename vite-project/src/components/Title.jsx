ebc81ae6b39b4ef4ecf027eeb71833e2c9ca48ba
Yuuki Aizato <yuukishuuto160318@gmail.com>
2024-01-11 05:08:54 +0900

    タイトル部分の追加

---
+++ vite-project/src/components/Title.jsx
@@ -0,0 +1,10 @@
+const Title = () => {
+    return(
+        <header>
+            <h1>画像検索</h1>
+            <p><a href="https://unsplash.com/">UnsplashApi</a></p>
+        </header>
+    );
+}
+ 
+export default Title;
